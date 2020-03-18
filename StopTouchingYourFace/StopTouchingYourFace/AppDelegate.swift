787ce05904229f90444443bec8a1970335661858
Matthew Spear <matthewtimothyspear@gmail.com>
2020-03-18 17:48:28 +0000

    Setup project, gitignore, swiftformat

---
+++ StopTouchingYourFace/StopTouchingYourFace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StopTouchingYourFace
+//
+//  Created by Matthew Spear on 18/03/2020.
+//  Copyright © 2020 Matthew Spear. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StopTouchingYourFace/StopTouchingYourFace/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StopTouchingYourFace
+//
+//  Created by Matthew Spear on 18/03/2020.
+//  Copyright © 2020 Matthew Spear. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
